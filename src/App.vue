20c7d8fcd57b43edaa7e5e53b10e08b9f9ab5140
songzhanpeng <songzhanpeng@iauto.com>
2023-09-05 11:20:25 +0800

    feat: 修改

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import QuickSearch from "./components/QuickSearch.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <QuickSearch />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+* {
+  padding: 0;
+  margin: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+html,
+body,
+#app {
+  height: 100vh;
 }
 </style>
---
+++ src/components/QuickSearch.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="quick-warper">
+    <!-- 图标 -->
+    <div class="icon">
+      <el-select
+        v-model="searchEngine"
+        class="m-2"
+        placeholder="Select"
+        size="large"
+      >
+        <el-option
+          v-for="item in searchEngineJson"
+          :key="item.name"
+          :label="item.name"
+          :value="item.link"
+        />
+      </el-select>
+    </div>
+    <!-- 搜索组件 -->
+    <div class="search">
+      <el-input
+        v-model="searchInp"
+        placeholder="输入关键词搜索 | tab键切换引擎"
+        @keyup.enter="handleEnterKey"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import searchEngineJson from "@/data/engine.json";
+
+const searchInp = ref<string>("");
+const searchEngine = ref<string>(searchEngineJson[0].link);
+
+const handleEnterKey = () => {
+  const link = searchEngine.value + searchInp.value;
+  window.open(link);
+};
+</script>
+
+<style scoped lang="scss">
+.quick-warper {
+  display: flex;
+  align-items: center;
+  width: 300px;
+
+  .icon {
+    width: 60px;
+  }
+
+  .search {
+    flex: 1;
+  }
+}
+</style>
